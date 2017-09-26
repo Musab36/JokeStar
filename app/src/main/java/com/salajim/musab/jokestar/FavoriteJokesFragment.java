bbac38bc1a39428cbbf1be1a76cf57e2247721d3
Musab36 <musabhd@gmail.com>
2017-09-26 15:47:32 +0300

    Basic setup, fragments.

---
+++ app/src/main/java/com/salajim/musab/jokestar/FavoriteJokesFragment.java
@@ -0,0 +1,17 @@
+package com.salajim.musab.jokestar;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FavoriteJokesFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.favorite_jokes, container, false);
+    }
+}
